9142e74125188f264453f5875ab157b4b07a3cd1
Azamat Uzbekov <batukasss@gmail.com>
2021-11-17 21:37:14 -0600

    list of users data displaied

---
+++ src/components/UserList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="row">
+            
+        </div>
+        <div v-for="(user, index) in users" :key="index">
+            <div class="card" style="width: 18rem;">
+                <img :src="user.avatar_url" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{user.name}}</h5>
+                    <p class="card-text">{{user.bio}}</p>
+                    <a :href="user.html_url" class="btn btn-primary" target="_blank">Go to GitHub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'user-list',
+        props: ['users', 'user']
+    }
+</script>
